0359bb00acc89a673ba6a86241f3fba883862150
mini-jail <mini-jail@proton.me>
2023-03-20 09:14:54 +0100

    :3

--- src/components/use-wiki.ts
+++ src/components/use-wiki.ts
@@ -8,11 +8,8 @@ export function useWiki(id: string, trigger: () => boolean) {
     if (trigger() === false) return wiki(id);
     if (cache.has(id)) return wiki(cache.get(id));
     const res = await fetch(`https://danbooru.donmai.us/wiki_pages/${id}.json`);
-    if (res.status === 200) {
-      cache.set(id, (await res.json()).body);
-    } else {
-      cache.set(id, id);
-    }
+    if (res.status === 200) cache.set(id, (await res.json()).body);
+    else cache.set(id, id);
   }));
   return wiki;
 }
